761e19eb2129e6e483a6219e016b44ebf5069356
seuzyf <60094910+seuzyf@users.noreply.github.com>
2026-06-26 16:38:41 +0800

    Update getScores.js

--- netlify/functions/getScores.js
+++ netlify/functions/getScores.js
@@ -3,8 +3,8 @@ const { createClient } = require('@supabase/supabase-js');
 exports.handler = async () => {
     try {
         const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
-        // 修改了查询字段，新增了玩家姓名、游戏ID和位置
-        const { data, error } = await supabase.from('player_stats').select('player_name, game_id, player_role, hw_score');
+        // 修改为查询S6专属表
+        const { data, error } = await supabase.from('s6_player_stats').select('player_name, game_id, player_role, hw_score');
         
         if (error) throw error;
         return { statusCode: 200, body: JSON.stringify(data) };
